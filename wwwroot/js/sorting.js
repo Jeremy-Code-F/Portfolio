a85e502a66adffb7e3e3614a39534bb412820188
Jeremy Farmer <jeremy.codef@gmail.com>
2020-01-12 14:21:23 -0500

    Finished bubble sort

--- wwwroot/js/sorting.js
+++ wwwroot/js/sorting.js
@@ -1,5 +1,6 @@
 var arrayToSort = [];
 var numberOfNumbers = 100;
+var waitTime = 100;
 window.addEventListener('DOMContentLoaded', (event) => {
 
     //Generate an array with n elements random number
@@ -19,97 +20,51 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 });
 
-function blaBubble(){
-    let barContainer = document.getElementById("barHolder");
-    let arr = barContainer.children; 
-    let n = arr.length;
-    for(let i = 0; i < n - 1; i++){
-        for(let j = 0; j < n - i - 1; j++){
-            let temp = arrayToSort[j];
-            arrayToSort[j] = arrayToSort[j + 1];
-            arrayToSort[j + 1] = temp;
-        }
-    }
+function bubbleSort(){
+    bubbleSortLoad();
 }
 
+function timer() {
+    ms = waitTime;
+    if(waitTime === 0){
+        return;
+    }
+    return new Promise(res => setTimeout(res, ms));
+}
 
-function testBubbleSort(){
+   
+async function bubbleSortLoad () { // We need to wrap the loop into an async function for this to work
     let barContainer = document.getElementById("barHolder");
     let arr = barContainer.children; 
     let n = arr.length;
     for(let i = 0; i < n - 1; i++){
+        await timer(waitTime);
         for(let j = 0; j < n - i - 1; j++){
+            //await timer(waitTime);
             if(parseInt(arr[j].id) > parseInt(arr[j + 1].id)){
+                arr[j].style.backgroundColor = "green";
+                arr[j + 1].style.backgroundColor = "green";
+                //await timer(waitTime);
                 let nextElem = document.getElementById(j+1);
-                console.log("Swapping: " + arr[j].id + " and : " + arr[j + 1].id);
 
                 let tempHeight = arr[j].style.height;
                 let tempId = arr[j].id;
 
                 let nextHeight = arr[j + 1].style.height;
                 let nextId = arr[j + 1].id;
-
+                
+                await timer(waitTime);
                 arr[j].style.height = nextHeight;
                 arr[j].id = nextId;
 
+                await timer(waitTime);
                 arr[j + 1].style.height = tempHeight;
                 arr[j + 1].id = tempId;
 
+                arr[j].style.backgroundColor = "blue";
+                arr[j + 1].style.backgroundColor = "blue";
 
             }
         }
     }
 }
-
-
-function bubbleSort(){
-    //DO a bubble sort on the arrayToSort
-    //load();
-    let n = arrayToSort.length;
-    for(let i = 0; i < n - 1; i++){
-        for(let j = 0; j < n - i - 1; j++){
-            if(arrayToSort[j] > arrayToSort[j + 1]){
-                let elemSearch = j + "_" + arrayToSort[j];
-                let currentBar = document.getElementById(j + "_" + arrayToSort[j]);
-                let nextBar = document.getElementById(j + 1 + "_" + arrayToSort[j + 1]);
-                currentBar.style.backgroundColor = "green";
-                nextBar.style.backgroundColor = "green";
-
-                let tempElement = currentBar.style.height;
-                let tempID = currentBar.id;
-
-                currentBar.style.height = nextBar.style.height;
-                currentBar.id = nextBar.id;
-
-                nextBar.style.height = tempElement;
-                nextBar.id = tempID;
-
-                let temp = arrayToSort[j];
-                arrayToSort[j] = arrayToSort[j + 1];
-                arrayToSort[j + 1] = temp;
-            }
-        }
-
-    }
-
-    for(let i = 0; i < arrayToSort.length; i++){
-        console.log(arrayToSort[i]);
-    }
-    console.log("Done bubble sort");
-
-
-    return 0;
-}
-
-function timer(ms) {
-    return new Promise(res => setTimeout(res, ms));
-   }
-   
-   async function load () { // We need to wrap the loop into an async function for this to work
-    for(let i = 0; i < arrayToSort.length; i++){
-        let currentBar = document.getElementById(i + "_" + arrayToSort[i]);
-        currentBar.style.backgroundColor = "green";
-        await timer(50);
-        currentBar.style.backgroundColor = "blue";
-    }
-   }
